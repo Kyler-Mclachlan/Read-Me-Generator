0fc6038fccb40ac109527230bdc0484f131b7a64
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2022-01-22 13:59:32 -0600

    added basic format

---
+++ Develop/index.js
@@ -0,0 +1,72 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+const { title } = require('process');
+
+// TODO: Create an array of questions for user input
+const userInput = () => {
+    
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: titleInput => {
+          if (titleInput) {
+            return true;
+          } else {
+            console.log('please enter your projects title!')
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide project description'
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter your projects installation instructions'
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage information:'
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'Provide contribution guidelines:'
+      },
+      {
+        type: 'input',
+        name: 'testing',
+        message: 'Provide test instructions'
+      }
+    ])   
+  };
+
+
+// TODO: Create a function to write README file
+function writeToFile(readMeTemplate, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+userInput()
+    .then(projectDetails => {
+        const readMefile = generateMarkdown(projectDetails)
+            fs.writeFile('./readMEoutput.md', readMefile, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+      console.log(projectDetails)
+      console.log(projectDetails.title)
+    });
+    });
---
+++ Develop/readMEoutput.md
@@ -0,0 +1,12 @@
+# 
+  
+  MyProject
+
+  This is a test project
+  
+  enter in a few words
+
+  please email me at kyler.mclachlan@gmail.com
+
+  test away!
+ 
